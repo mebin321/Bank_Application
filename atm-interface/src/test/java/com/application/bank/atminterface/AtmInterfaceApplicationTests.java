69674896d0c6a5196237a75c53ad898d557c50eb
Lenovo <Lenovo@LAPTOP-94VSOB3U>
2021-09-20 13:40:46 +0530

    First Commit

---
+++ atm-interface/src/test/java/com/application/bank/atminterface/AtmInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.bank.atminterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
